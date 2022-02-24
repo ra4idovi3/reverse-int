bbb39851aec3f39190050e9dc064c79c28bf4d2e
ra4idovi3 <al.shportak@gmail.com>
2022-02-24 19:49:33 +0500

    feat: add reverse fucntion. All tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let newArr = n.toString().split('');
+    let newStr = '';
+        for (let i = newArr.length - 1; i>=0; i--){
+            newStr += newArr[i];
+        }
+    return newStr.replace('-','');
 }
